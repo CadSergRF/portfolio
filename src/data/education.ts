f01dfa3b6073338fedb379e7f05cbc583476cf62
Sergey Evdokimov <cadsergrf@yandex.ru>
2024-02-08 10:47:57 +0700

    del univ

--- src/data/education.ts
+++ src/data/education.ts
@@ -5,9 +5,9 @@ export const education = [
     dates: 'сентябрь 2022 - август 2023',
     text: 'Обучение по технологиям и направлениям: HTML, CSS, SCSS, JavaScript, адаптивная и кросс-браузерная верстка, БЭМ, создание приложений с использованием React, Redux Toolkit, TypeScript, NodeJS, ExpressJS, MongoDB',
   },
-  {
-    position: 'Инженер электронной техники',
-    company: 'ТУСУР',
-    dates: '1997 - 2002',
-  },
+  // {
+  //   position: 'Инженер электронной техники',
+  //   company: 'ТУСУР',
+  //   dates: '1997 - 2002',
+  // },
 ];
