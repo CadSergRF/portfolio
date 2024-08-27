04f57e2d95e981c2980a1c5d011217b1bbfc9871
Пользователь <cadsergrf@yandex.ru>
2024-08-27 15:27:53 +0700

    init: bug fix portfolio

--- src/components/Portfolio/Card/Card.tsx
+++ src/components/Portfolio/Card/Card.tsx
@@ -15,7 +15,15 @@ const Card = ({ image, name, linkDemo, linkCode }: TCard) => {
   return (
     <div className={styles.container}>
       <div className={styles.image__container}>
-        <Image priority={true} src={image} fill alt="Превью приложения" className={styles.image} />
+        <img src={image} className={styles.image}/>
+        {/* <Image
+        priority={true}
+        src={image}
+        fill
+        sizes="100%"
+        alt="Превью приложения"
+        className={styles.image}
+        /> */}
       </div>
       <div className={styles.name__container}>
         <h3 className={styles.name}>{name}</h3>
--- src/data/education.ts
+++ src/data/education.ts
@@ -1,13 +1,14 @@
 export const education = [
+    {
+    position: 'Разработчик на JS+React',
+    company: 'Е1 Цифровая академия',
+    dates: 'июль 2024',
+  },
   {
     position: 'Frontend разработчик',
     company: 'Яндекс.Практикум',
     dates: 'сентябрь 2022 - август 2023',
     text: 'Обучение по технологиям и направлениям: HTML, CSS, SCSS, JavaScript, адаптивная и кросс-браузерная верстка, БЭМ, создание приложений с использованием React, Redux Toolkit, TypeScript, NodeJS, ExpressJS, MongoDB',
   },
-  // {
-  //   position: 'Инженер электронной техники',
-  //   company: 'ТУСУР',
-  //   dates: '1997 - 2002',
-  // },
+
 ];
